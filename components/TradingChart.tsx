f506917537c371a4a695b99db1bd79ef79154437
Kiruivenus <patrickkirui413@gmail.com>
2026-09-14 18:12:55 +0300

    Equalize Left positions card and TradingChart graph container height to h-480px

--- components/TradingChart.tsx
+++ components/TradingChart.tsx
@@ -257,7 +257,7 @@ export default function TradingChart({
   const digitPercentages = digitCounts.map((c) => ((c / total) * 100).toFixed(1));
 
   return (
-    <div className="relative bg-[#141722] border border-slate-800/80 rounded-2xl overflow-hidden shadow-2xl flex flex-col h-[500px] sm:h-[550px]">
+    <div className="relative bg-[#141722] border border-slate-800/80 rounded-2xl overflow-hidden shadow-2xl flex flex-col h-[460px] sm:h-[480px]">
       {/* 1. TOP-LEFT FLOATING INSTRUMENT SELECTOR CARD (Image 1 Match) */}
       <div className="absolute top-3 left-3 z-30">
         <div
